eb1fd47c8cabf7250a640ff3fcd9970ce2e84f80
Optima__ <zxcqlag@gmail.com>
2025-09-12 14:28:45 +0300

    correct jar compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.1.21"
     kotlin("plugin.serialization") version "2.0.20"
+    `java-library`
 }
 
 group = "org.example"
@@ -18,4 +19,9 @@ dependencies {
 
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.jar {
+    archiveBaseName.set("config-loader")
+    from(sourceSets.main.get().output)
 }
