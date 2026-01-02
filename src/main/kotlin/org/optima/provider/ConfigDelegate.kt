5a2ea3dc893e3ecbf62b37891622d721f13f87e3
Optima__ <zxcqlag@gmail.com>
2026-01-02 19:12:45 +0300

    added delegate variants

--- src/main/kotlin/org/optima/provider/ConfigDelegate.kt
+++ src/main/kotlin/org/optima/provider/ConfigDelegate.kt
@@ -3,13 +3,36 @@ package org.optima.provider
 import org.optima.ConfigServiceHolder
 import kotlin.reflect.KProperty
 
-class ConfigDelegate<T : Any>(private val clazz: Class<T>) {
+class AutoConfigDelegate<T : Any>(
+    private val clazz: Class<T>
+) {
     operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
         val serviceKey = ConfigRegistry.getServiceKey(clazz)
         val service = ConfigServiceHolder.getService(serviceKey)
-        val name = clazz.simpleName.replaceFirstChar { it.lowercase() }
+        return service.getConfig(clazz.toSnakeCase())
+    }
+}
+
+fun Class<*>.toSnakeCase(): String =
+    simpleName
+        .replace(Regex("([a-z])([A-Z])"), "$1_$2")
+        .lowercase()
+
+class NamedConfigDelegate<T : Any>(
+    private val name: String,
+    private val clazz: Class<T>
+) {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        val serviceKey = ConfigRegistry.getServiceKey(clazz)
+        val service = ConfigServiceHolder.getService(serviceKey)
         return service.getConfig(name)
     }
 }
 
-inline fun <reified T : Any> config() = ConfigDelegate(T::class.java)
+inline fun <reified T : Any> config() =
+    AutoConfigDelegate(T::class.java)
+
+inline fun <reified T : Any> config(name: String) =
+    NamedConfigDelegate(name, T::class.java)
+
+
